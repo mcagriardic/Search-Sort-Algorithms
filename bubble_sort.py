1dea30aa703d8d1105c15b09f2debaf6a785c9a1
mcagriardic <cagri_ardic@hotmail.com>
2020-06-27 11:36:35 +0100

    Fixed early exit logic

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,17 +6,21 @@ class BubbleSort:
     def __init__(self, lst):
         self.lst = lst
         
+        self.count = 0
+        
     def sort(self):
         for i in range(len(self.lst)):
             swap = False
             for j in range(len(self.lst) - 1):
+                self.count += 1
                 if self.lst[j] > self.lst[j + 1]:
                     swap = True
                     self.lst[j], self.lst[j+1] = self.lst[j+1], self.lst[j]
             
-            if not swap:
-                print("Sorted list: %s" %self.lst)
-                return 1
+                if not swap:
+                    print("Took %s steps" %self.count)
+                    print("Sorted list: %s" %self.lst)
+                    return 1
 
 sort_instance = BubbleSort([3,1,5,6,2,7,5,6,3,5,9,5,55,2,4,8,9,6,2,20])
 sort_instance.sort()
