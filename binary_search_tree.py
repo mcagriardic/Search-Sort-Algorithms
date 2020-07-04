ef7c356e07879cbefe74cbadfdbc3d2d4ddec579
mcagriardic <cagri_ardic@hotmail.com>
2020-07-04 19:18:15 +0100

    added BinarySearchTree example

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -88,3 +88,72 @@ class BinarySearchTree(object):
         self.traverse(root.right)
         if postorder:
             print(root.val)
+
+
+### EXAMPLE ###
+## Construct the tree
+
+#                   50
+#             /               \
+#            25               75
+#         /      \         /      \
+#        10      33       56      89
+#       / \     /  \     /  \    /  \
+#      4  11   30  40   52  61  82  95
+
+##
+
+left_1 = TreeNode(25)
+right_1 = TreeNode(75)
+
+left_1_left = TreeNode(10)
+left_1_right = TreeNode(33)
+
+left_1_left_left = TreeNode(4)
+left_1_left_right = TreeNode(11)
+
+left_1_right_left = TreeNode(30)
+left_1_right_right = TreeNode(40)
+
+right_1_left = TreeNode(56)
+right_1_right = TreeNode(89)
+
+right_1_left_left = TreeNode(52)
+right_1_left_right = TreeNode(61)
+
+right_1_right_left = TreeNode(82)
+right_1_right_right = TreeNode(95)
+
+root = TreeNode(50, left_1, right_1)
+
+left_1.left = left_1_left
+left_1.right = left_1_right
+
+left_1_left.left = left_1_left_left
+left_1_left.right = left_1_left_right
+
+left_1_right.left = left_1_right_left 
+left_1.right.right = left_1_right_right
+
+
+right_1.left = right_1_left
+right_1.right = right_1_right
+
+right_1_left.left = right_1_left_left
+right_1_left.right = right_1_left_right
+
+right_1_right.left = right_1_right_left
+right_1_right.right = right_1_right_right
+
+# Test search, insert, dekete abnd traverse methods
+
+bst = BinarySearchTree(root)
+bst.search(55, root)
+
+bst.insert(43, root)
+bst.traverse(root)
+
+root = bst.delete(50, root)
+bst.traverse(root)
+
+
