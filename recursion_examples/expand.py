3ff1505f14c89ce861b2e649e1b4091a83b85eb8
mcagriardic <cagri_ardic@hotmail.com>
2020-11-24 14:40:39 +0000

    added recursion folder

---
+++ recursion_examples/expand.py
@@ -0,0 +1,17 @@
+def expand(cs, to_append=""):
+    expanded = ""
+    
+    if "[" not in cs:
+        return cs
+    
+    for i in range(len(cs)):
+        if cs[i].isdigit():
+            no = int(cs[i])
+        elif cs[i].isalpha():
+            to_append += cs[i] 
+        elif cs[i] == "[":
+            return to_append + no * expand(cs[i+1:-1])
+
+    return expanded
+
+expand("2[a2[b]a2[c]]")
---
+++ recursion_examples/flatten.py
@@ -0,0 +1,15 @@
+def flatten(lst):
+  flattened = []
+
+  if not lst:
+    return None
+  
+  for elem in lst:
+    if not isinstance(elem, list):
+      flattened.append(elem)
+    elif isinstance(elem, list):
+      flattened.extend(flatten(elem))
+  
+  return flattened
+
+  flatten([1,2,3,[4,5, [6,7,8]]])
