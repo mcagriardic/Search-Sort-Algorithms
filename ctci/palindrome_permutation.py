003779addbb6fcec75d07f15fa2f0dadc266e101
mcagriardic <cagri_ardic@hotmail.com>
2021-01-01 20:24:35 +0000

    added is palindrome permutation

---
+++ ctci/palindrome_permutation.py
@@ -0,0 +1,22 @@
+"""
+From: Cracking the Coding Interview
+Check if a string is a permutation of a palindrome.
+Palindrome is defines as a string which is the same backwards as it is forwards (taco cat, madam)
+Permutation is the letters in the string scrambled.
+"""
+
+from collections import defaultdict
+
+def is_palindrome_permutation(s):
+    s = s.split(" ")
+    s1, s2 = s[0], s[1]
+
+    d2 = defaultdict(lambda: 0)
+    for c2 in s2:
+        d2[c2] += 1
+
+    for c1 in s1:
+        if c1 in d2:
+            d2[c1] -= 1
+
+    return sum(d2.values()) == 0
