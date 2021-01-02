24f7eba27221daeddaf9f9b43833dc49815615e5
mcagriardic <cagri_ardic@hotmail.com>
2021-01-02 12:46:38 +0000

    added pattern_matching.py

---
+++ ctci/pattern_matching.py
@@ -0,0 +1,66 @@
+"""
+From: Cracking the Coding Interview
+Pattern Matching
+You are given two strings, pattern and value.
+The pattern can only be a and b where as values can be for example
+catcatcatgocatgo which matches with aaabab. Write a function which tests
+to see if the value matches the pattern.
+"""
+
+def find_pattern(cs):
+    if len(cs) == 0:
+        return ""
+
+    # p -> pattern
+    p = [cs[0]]
+    # tc -> temporary container
+    tc = []
+    # mi -> match index
+    mi = 0
+    for i in range(1, len(cs)):
+        if cs[i] == p[mi]:
+            tc.append(cs[i])
+            mi += 1
+            if p == tc:
+                return "".join(p)
+        elif cs[i] != p[mi]:
+            p.append(cs[i])
+    return "".join(p)
+
+def count_until_next_dif_elem(p):
+    c = 1
+    for i in range(1, len(p)):
+        if p[i-1] == p[i]:
+            c += 1
+        else:
+            break
+    return c
+
+
+def is_pattern_match(cs, pm):
+    s = 0
+    while True:
+        p = find_pattern(cs)
+        cut = count_until_next_dif_elem(pm)
+        if p * cut != cs[s:len(p) * cut]:
+            return False
+
+        pm = pm[cut:]
+        cs = cs[len(p) * cut:]
+        
+        if len(cs) == 0:
+            break
+    return True
+
+# tc -> test cases
+tc = [
+    ("catcatcatgogocat", "aaabba"),
+    ("catcatcacar", "aabb"),
+    ("catcaxcaca", "aabb")
+]
+
+[is_pattern_match(cs, pm) for cs, pm in tc]
+
+"""
+>> [True, True, False]
+"""
