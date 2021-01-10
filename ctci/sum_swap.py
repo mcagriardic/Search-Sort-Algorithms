597304e5065626c45ce8c33503235372cb92665d
mcagriardic <cagri_ardic@hotmail.com>
2021-01-10 11:27:56 +0000

    added sum_swpa

--- ctci/sum_swap.py
+++ ctci/sum_swap.py
@@ -9,3 +9,26 @@ Example:
             [3, 6, 3, 3]
     output: [1, 3]
 """
+
+def get_sum_and_unique_elems(ns):
+    sum_ = 0
+    set_ = set()
+    for n in ns:
+        sum_ += n
+        set_.add(n)
+    return sum_, set_
+
+def sum_swap(ns1, ns2):
+    sum1, set1 = get_sum_and_unique_elems(ns1)
+    sum2, set2 = get_sum_and_unique_elems(ns2)
+    equal_sum = (sum1 + sum2) / 2
+
+    for n1 in set1:
+        if equal_sum - (sum1 - n1) in set2:
+            yield n1, int(equal_sum - (sum1 - n1))
+
+list(sum_swap([4, 1, 2, 1, 1, 2], [3, 6, 3, 3]))
+
+"""
+>> [(1, 3), (4, 6)]
+"""
